555a2c35d294f58a41a0711f92338ec50083e982
Onyenuwe Japheth <japhheth@gmail.com>
2019-10-23 17:22:09 +0100

    Completed Module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Album } from './album';
 
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
@@ -12,8 +14,8 @@ export class ProductService {
   
   }
 
-  getAlbum( id: number){
-    return this._http.get(this._albumUrl).map((response) => response.json());
+  getAlbum( id: number) : Observable<Album>{
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 
 }
